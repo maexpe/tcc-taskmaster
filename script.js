8dbfca7446557f8892dcd6caad6bc039461d7c25
maexpe <matheusexpedito58@gmail.com>
2024-04-27 16:38:12 -0300

    feat: Adição de função de remover tarefa

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        
+        let span = document.createElement("span");
+        span.innerHTML = "✖";
+        li.appendChild(span);
     }
+    inputBox.value = "";
 }
 
 document.addEventListener('keydown', function(event) {
@@ -17,3 +22,11 @@ document.addEventListener('keydown', function(event) {
         addTask();
     }
 });
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
